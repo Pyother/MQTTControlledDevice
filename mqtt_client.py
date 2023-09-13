66e5d5381f658784af740a44edcebef776f3a5d2
pyother <piotr.dominik.sobol@gmail.com>
2023-09-13 10:24:20 +0200

    Establish Raspberry Pi & Backend & Frontend callbacks and data exchange

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,13 +1,29 @@
 import paho.mqtt.client as mqtt
-from blink_led import blink_led
+from wifitest import wifitest
+#from blink_led import blink_led
 
 def on_connect(client, userdata, flags, rc): 
     print("Connected with result code "+str(rc))
-    client.subscribe("measurements")
+    client.subscribe("AreaExplorer")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    blink_led()
+    message = (str(msg.payload))[2:-1]
+    print(message)
+
+    if "speedtest" in message: 
+        print("→ Wifi test request received")
+        client.publish(topic="AreaExplorer", payload=str(wifitest()))
+
+    if "drive" in message:
+        print("→ Drive request received")
+
+    if "carbon_monoxide_measurement" in message:
+        print("→ Carbon Monoxide measurement request received")
+
+    if "methane_measurement" in message:
+        print("→ Methane measurement request received")
+
+    #blink_led()
 
 client = mqtt.Client()
 client.on_connect = on_connect
--- wifitest.py
+++ wifitest.py
@@ -2,11 +2,16 @@ import speedtest
 
 wifi = speedtest.Speedtest()
 
-print("Getting download speed")
-download_speed = wifi.download()
+def wifitest():
+    print("Getting download speed ...")
+    download_speed = wifi.download()
 
-print("Getting upload speed")
-upload_speed = wifi.upload()
+    print("Getting upload speed ...")
+    upload_speed = wifi.upload()
 
-print("Download: ", download_speed)
-print("Upload: ", upload_speed)
+    print("Download: ", download_speed)
+    print("Upload: ", upload_speed)
+    
+    message = "wifitest_callback:" + str(download_speed) + ":" + str(upload_speed)
+
+    return (message)
