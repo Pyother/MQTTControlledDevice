0386b76e7398250a41e603b96671962e05045ee5
Piotr <77791657+Pyother@users.noreply.github.com>
2023-11-21 22:10:54 +0100

    Handle servo

---
+++ servo.py
@@ -0,0 +1,23 @@
+
+from gpiozero import Servo
+from gpiozero import AngularServo
+
+#servo = Servo(13)
+s = AngularServo(13)
+
+def turn(direction):
+
+        initial_value = 6
+        angle = 60
+
+        if direction == "right":
+                #servo.value = -1
+                s.angle = initial_value - angle
+
+        if direction == "left":
+                #servo.value = 1
+                s.angle = initial_value + angle
+
+        if direction == "initial":
+                #servo.value = 0
+                s.angle = initial_value
